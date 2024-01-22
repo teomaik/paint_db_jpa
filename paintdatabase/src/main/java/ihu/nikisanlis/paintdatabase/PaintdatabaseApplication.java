4f2c6626c10007e81ade7e24c13daccc059bad2f
Theodore Maikantis <teomaik19@gmail.com>
2024-01-22 22:54:27 +0200

    Spring initialzr code

---
+++ paintdatabase/src/main/java/ihu/nikisanlis/paintdatabase/PaintdatabaseApplication.java
@@ -0,0 +1,13 @@
+package ihu.nikisanlis.paintdatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaintdatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaintdatabaseApplication.class, args);
+	}
+
+}
---
+++ paintdatabase/src/test/java/ihu/nikisanlis/paintdatabase/PaintdatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package ihu.nikisanlis.paintdatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaintdatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
